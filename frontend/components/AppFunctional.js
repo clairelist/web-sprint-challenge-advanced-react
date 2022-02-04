0c7861e33091015cd180a2dbe8fa0c1ac89b1e94
Claire List <claire.list2501@gmail.com>
2022-02-03 18:06:09 -0700

    working thru the logic

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react';
+
+let x = 2
+let y = 2
+const initialCoords = {x,y};
+
+//okay so for this first attempt at the logic, let's think about it as incrementing each value
+//and then destructuring that array to correspond with which square gets the big ole B rendered to it
+
+//these will be moved to an actions file when ready !
+//increments:: state changers !
+const incrementX = function(state,x){
+  if(x<=3){
+    state = setState(x+1)
+  } else {
+   state = setState(x);
+  }
+}
+
+const incrementY = function(state,y){
+  if(y<=3){
+    state = setState(y+1)
+  } else {
+   state = setState(y);
+  }
+}
+
+//decrements !
+const decrementX = function(state,x){
+  if(x>=1){
+    state = setState(x-1);
+  } else {
+    state = setState(x);
+  }
+}
+
+const decrementY = function(state,y){
+  if(y>=1){
+    state = setState(y-1);
+  } else {
+    state = setState(y);
+  }
+}
+
+//click handlers ! Calling the above actions, which are currently above as normal funcs!
+const clickUp = function(event){
+  event.preventDefault();
+  incrementY();
+}
+
+const clickRight = function(event){
+  event.preventDefault();
+  incrementX();
+}
 
 export default function AppFunctional(props) {
   return (
